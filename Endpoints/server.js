21c2e64a255bcefe80a8902f904316e0d89696d0
Harold <haroldbarron13@gmail.com>
2022-05-15 02:29:32 -0500

    Creating server.js file to have all the necesary endpoints

---
+++ Endpoints/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+  res.json({message: 'alive'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
+
+app.get('/Users',async(req,res) =>
+{
+  const allUseres = await prisma.users.findMany({});
+  res.json(allUseres);
+})
+
+app.get('/Users/:user', async (req, res) => {
+  const user = req.params.user; 
+  console.log(user)
+  const oneUser = await prisma.users.findUnique({where: {user: user}});
+  res.json(oneUser);
+});
+
+app.post('/Users', async (req, res) => {
+  const newUser = {
+    user: req.body.user,
+    name: req.body.name,
+    regid:req.body.regid,
+    zoneLeader:false,
+    email: req.body.email,
+   };
+  const message = 'Usuario creado.';
+  await prisma.users.create({data: newUser});
+  return res.json({message});
+});
+
+app.put('/Users/:user', async (req, res) => {
+	const userToUpdate = req.params.user;
+
+	await prisma.users.update({
+		where: {
+			user: userToUpdate
+		},
+		data: {
+			regid:req.body.regid,
+      zoneLeader: req.body.zoneLeader
+		}
+	})
+	return res.json({message: "Usuario actualizado correctamente"});
+});
+
+app.delete('/Users/:user', async (req, res) => {
+	const userToDelete = req.params.user;
+	await prisma.users.delete({where: {user: userToDelete}});
+	return res.json({message: "Usuario eliminado correctamente"});
+});
+
+app.get('/Animal',async(req,res) =>
+  {
+    const allUseres = await prisma.animals.findMany({});
+    res.json(allUseres);
+  })
