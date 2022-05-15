6c34b184d9b25dd788f36ddbd42a7b0baff11c4e
Harold <haroldbarron13@gmail.com>
2022-05-15 00:38:43 -0500

    Creation of the CRUD of UserService.js

---
+++ Endpoints/userServer.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+  res.json({message: 'alive'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
+
+app.get('/Users',async(req,res) =>
+{
+  const allUseres = await prisma.users.findMany({});
+  res.json(allUseres);
+})
+
