562a4bdb48baf8d440dc66b2a7614008623092ad
Harold <haroldbarron13@gmail.com>
2022-05-15 16:56:20 -0500

    Adding dotenv

--- Endpoints/server.js
+++ Endpoints/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 // Require para usar Prisma
 const { PrismaClient } = require("@prisma/client");
